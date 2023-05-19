ab80d5ccfd1b882ad8ea72c70c82139e42338f2f
jorj-kare <jorj.kare@gmail.com>
2023-05-19 19:04:06 +0200

    set alias and preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
-
+import preprocessor from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocessor({
+		scss: {
+			prependData: '@use "src/styles/_variables.scss" as *; '
+		}
+	}),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$lib: 'src/lib',
+			$components: 'src/lib/components',
+			$stores: 'src/lib/stores',
+			$utils: 'src/lib/utils'
+		}
 	}
 };
 
